94d2a41581a8f498b885d4eb41a5364c9878da4f
Vanya_Coffee <vanakupnevskij@gmail.com>
2022-10-21 18:42:31 +0300

    popup list

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -3,19 +3,52 @@ import { Link } from 'react-router-dom';
 import styles from './Filter.module.scss';
 
 function Filter() {
+  const [activeCategory, setActiveCategory] = React.useState(0);
+
+  const [isOpenSort, setOpenSort] = React.useState(false);
+  const [activeSort, setActiveSort] = React.useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  const sortes = ['популярности', 'цене', 'алфавиту'];
+  let selectedSort = sortes[activeSort];
+
+  function onClickCategory(id) {
+    setActiveCategory(id);
+  }
+
+  function onClickSort(id) {
+    setActiveSort(id);
+    setOpenSort(false);
+  }
+
   return (
     <section className={styles.section}>
       <ul className={styles.types}>
-        <li className={styles.types_selected}>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((item, ind) => (
+          <li
+            onClick={() => onClickCategory(ind)}
+            className={activeCategory == ind ? styles.types_selected : ''}>
+            {item}
+          </li>
+        ))}
       </ul>
       <div className={styles.characteristics}>
-        <span>Сортировка по:</span>
-        <span className={styles.charac}>популярности</span>
+        <div>
+          <img className={isOpenSort ? styles.img_opened : ''} src={'images/topdown.svg'} />
+          <span>Сортировка по:</span>
+          <span className={styles.charac} onClick={() => setOpenSort(!isOpenSort)}>
+            {selectedSort}
+          </span>
+        </div>
+        <ul className={[styles.sort_list, isOpenSort ? styles.popup_opened : ''].join(' ')}>
+          {sortes.map((item, ind) => (
+            <li
+              onClick={() => onClickSort(ind)}
+              className={activeSort == ind ? styles.sort_selected : ''}>
+              {item}
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
   );
